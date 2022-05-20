841fae5b1ecfb6522a5d3435f8aeaa009a98801e
Schtalf <Schtalf@bk.ru>
2022-05-20 12:50:52 +0200

    Dz3

---
+++ Homework/MRO.py
@@ -0,0 +1,26 @@
+# Алгоритм C3 поиска в дереве наследования классов Python 3 (MRO)
+
+class Vertebrate:
+    def __init__(self):
+        print('Vertebrate.__init__()')
+
+
+class Bird(Vertebrate):
+    def __init__(self):
+        print('Bird.__init__()')
+        super().__init__()
+
+
+class Mammal(Vertebrate):
+    def __init__(self):
+        print('Mammal.__init__()')
+        super().__init__()
+
+
+class Platypus(Mammal, Bird):
+    def __init__(self):
+        print('Platypus.__init__()')
+        super().__init__()
+
+
+duckbill = Platypus()
